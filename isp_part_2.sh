8867eac8de3d14fc9f6c8ff16a9b7a8fc1fcb014
NiKeNO1540 <66269872+NiKeNO1540@users.noreply.github.com>
2025-03-26 10:29:09 +0500

    Create isp_part_2.sh

---
+++ isp_part_2.sh
@@ -0,0 +1,31 @@
+# Магия вне хогвартса (Обновление пакетов через ssh на BR-RTR)
+
+echo "sudo apt-get update" | ssh branch_admin@44.44.44.2
+
+# Установка ansible && git
+
+apt-get install git -y
+apt-get install ansible -y
+
+# Настройка хостов
+
+cat << EOF > /etc/ansible/hosts
+VMs:
+ hosts:
+  BR-RTR:
+   ansible_host: 44.44.44.2
+   ansible_user: branch_admin
+EOF
+
+# Вставка строк в ansible.cfg
+sed '10 a\
+ansible_python_interpreter=/usr/bin/python3\
+interpreter_python=auto_silent\
+ansible_host_key_checking=false' /etc/ansible/ansible.cfg > ansible.cfg
+rm -rf /etc/ansible/ansible.cfg
+mv ansible.cfg /etc/ansible/ansible.cfg
+
+# Копирование и активация PlayBook-a
+
+cp /root/DEMO-2024-TESTING/BR-RTR.yml /root/BR-RTR.yml
+ansible-playbook BR-RTR.yml
