1f84c66f81f0a4ec9a1b450a6e41112ec03a1b09
NiKeNO1540 <66269872+NiKeNO1540@users.noreply.github.com>
2025-06-18 14:29:20 +0500

    Create Mediawiki.sh

---
+++ Mediawiki.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+apt-get install -y docker-ce docker-compose –y
+systemctl enable --now docker.service
+
+mv DEMO-2024-TESTING/LocalSettings.php ~/
+
+docker volume create dbvolume
+docker compose -f DEMO-2024-TESTING/wiki.yml up -d
